9361fac43a3e2e751fb7de970b1b0dc768571026
JustinFlo <225jflores@frhsd.com>
2024-12-05 11:43:55 -0500

    Added if-else

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-
-
 $(document).ready(function(){
     
-//var schduleDays = 'https://jsonkeeper.com/b/GMRE'
-
-$('#submitDay').on('click', function(){
-        
     
+    const scedueleList = $('#scheduleList')
+    const button = $('#submitDay')
+
+$('#submitDay').click (()=> {
+        var selectedDay = $('#dayInput').val()
+    
+        if(!['A', 'B', 'C','D', 'E', 'F', 'G'].includes(selectedDay)){
+            alert('Please enter a valid day(A-G)');
+            return;
+        }   else {
+            $.ajax({
+                url: 'https://api.npoint.io/0d8f38413549540fa060',
+                method: 'GET',
+                success: function (data)
+            })
+        }
+
 })
 })
 
