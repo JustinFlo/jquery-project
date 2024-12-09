a214939566174fb6f4f12b7b6fde30bd90cfc825
JustinFlo <225jflores@frhsd.com>
2024-12-09 09:29:08 -0500

    Added in rest of code, got the error, but doesn't put out schedule

---
+++ schedule.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    
+    
+    const scheduleList = $('#scheduleList')
+    const button = $('#submitDay')
+
+        button.on('click', function () {
+
+        
+       let selectedDay = $('#dayInput').val().trim().toUpperCase()
+            console.log(selectedDay)
+        if(!['A', 'B', 'C','D', 'E', 'F', 'G'].includes(selectedDay)){
+            alert('Please enter a valid day(A-G)');
+            return;
+        }   
+        else {
+            $.ajax({
+                url: 'https://api.npoint.io/fcdff2bd2e89ff21e4a5',
+                method: 'GET',
+                success: function (data) {
+                    const schedule = data.schedule
+                    console.log(schedule)
+
+                    const daySchedule = schedule.filter(item => item.days.includes(selectedDay))
+                    console.log(daySchedule)
+
+                    renderHTML(daySchedule)
+                }
+            })
+        }
+
+        function renderHTML(daySchedule){
+            $('#scheduleList').empty()
+            let htmlString = ''
+            daySchedule.forEach(function (classItem) {
+                htmlString += (`
+                    <tr>
+                    <td>${classItem.room}</td>
+                    <td>${classItem.class}</td>
+                    <td>${classItem.period}</td>
+                    <td>${classItem.teacher}</td>
+                    </td>`)
+            })
+
+            $('#scheduleList').append(htmlString);
+        }
+
+})
+
+
+})
+
